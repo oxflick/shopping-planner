7b287d8f5c6fba30d63507251705ad552e86018a
Oxflick <flick@funkyflick.com>
2016-09-22 21:40:43 -0700

    Logic and shopping planner list added

--- app.js
+++ app.js
@@ -6,9 +6,84 @@
 angular.module('checkOffApp', [])
 
 //this is a controller that responsible for our view and it bounds to the div in html file
-.controller('MyFirstController', function ($scope) {
-	
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-});
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+  var itemToBuy = this;
+  itemToBuy.items = ShoppingListCheckOffService.getItems();
+
+  itemToBuy.removeItem = function (itemIndex) {
+    try {
+      ShoppingListCheckOffService.removeItem(itemIndex);
+    }  catch (error) {
+      itemToBuy.errorMessage = error.message;
+    }
+  };
+}
+
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+  var showList = this;
+
+  showList.items = ShoppingListCheckOffService.showBoughtItems();
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // Shopping List of items to buy
+  var items = [
+  {
+  	name: "cookie",
+  	quantity: 2
+  },
+  {
+  	name: "tea",
+  	quantity: 1
+  },
+  {
+  	name: "banana",
+  	quantity: 5
+  },
+  {
+  	name: "bread",
+  	quantity: 1
+  },
+  {
+  	name: "tomatoes",
+  	quantity: 10
+  }
+
+  ];
+
+  // Bought list of shopping items 
+  var boughtitems = [];
+
+  // after button "bought" pressed remove the item from shopping list and add to the bought list
+  service.removeItem = function (itemIdex) {
+    boughtitems.push(items[itemIdex]);
+    items.splice(itemIdex, 1);
+    if (items.length == 0) {
+    throw new Error("Everything is bought");
+    }
+  };
+
+  //show items in To Buy List 
+  service.getItems = function () {
+    return items;
+  };
+
+  // show items in "Already Bought List"
+  service.showBoughtItems = function () {
+    return boughtitems;  
+  };
+
+  
+}
+
 
 })();
